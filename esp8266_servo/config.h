7267e193cea5d6f7ba05dc10f784f769759f0a1d
Inwooshin <59462895+inwooshin@users.noreply.github.com>
2021-01-11 21:11:11 +0900

    config 파일 수정

--- esp8266_servo/config.h
+++ esp8266_servo/config.h
@@ -2,8 +2,8 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME "EnochOf"
-#define IO_KEY "aio_ySSx377lvoFel6N8xjjbwcZbsxEL"
+#define IO_USERNAME ""
+#define IO_KEY ""
 
 /******************************* WIFI **************************************/
 
